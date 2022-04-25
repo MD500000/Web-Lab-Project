e03c73e154edbf1a65c2e44440108a6fb85ce79e
MD500000 <mddidi373@gmail.com>
2022-04-25 23:53:58 +0300

    -first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $("#slider").slick({
+
+        infinite: true,
+        slidesToShow:1,
+        slidesToScroll:3,
+        arrows: true,
+        dots:true,
+        autoplay:true,
+        centerMode: true
+
+    });
+        
+        
+})
